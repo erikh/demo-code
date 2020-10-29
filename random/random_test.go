3f92f1a550268ca8f7324621c63f0184e04ace11
Erik Hollensbe <github@hollensbe.org>
2020-10-29 07:02:18 -0700

    random string generator: tests and benchmarks
    Signed-off-by: Erik Hollensbe <github@hollensbe.org>

---
+++ random/random_test.go
@@ -0,0 +1,46 @@
+package random
+
+import (
+	"fmt"
+	"regexp"
+	"testing"
+)
+
+var stringRegex = regexp.MustCompile(`^[0-9A-Za-z]+$`)
+
+func TestRandom(t *testing.T) {
+	for i := 0; i < 1000; i++ {
+		s := String(100, 5)
+		if !stringRegex.MatchString(s) {
+			t.Fatalf("string did not match: %q", s)
+		}
+	}
+}
+
+func makeLenBench(b *testing.B, l uint) {
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			String(l, l)
+		}
+	})
+}
+
+func BenchmarkString5Len(b *testing.B) {
+	fmt.Println("String generation at length: 5")
+	makeLenBench(b, 5)
+}
+
+func BenchmarkString25Len(b *testing.B) {
+	fmt.Println("String generation at length: 5")
+	makeLenBench(b, 25)
+}
+
+func BenchmarkString100Len(b *testing.B) {
+	fmt.Println("String generation at length: 5")
+	makeLenBench(b, 100)
+}
+
+func BenchmarkString500Len(b *testing.B) {
+	fmt.Println("String generation at length: 5")
+	makeLenBench(b, 500)
+}
